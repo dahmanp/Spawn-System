8ba835ea0742cdbc57876ff5007372e792755992
dahmanp <dahmanp@my.erau.edu>
2024-10-18 06:33:03 -0700

    UI Implementation

--- Spawn System/Assets/Scripts/Client.cs
+++ Spawn System/Assets/Scripts/Client.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Client : MonoBehaviour
 {
+    public TMP_Text vehicleName;
+    public Toggle EngineToggle;
+    public Toggle CargoToggle;
+    public TMP_InputField wheelInput;
+    public TMP_InputField peopleInput;
+
     public int NumberOfWheels;
     public bool Engine;
     public int Passengers;
     public bool Cargo;
 
-    void Update()
+    public void VehicleButton()
     {
         /*if (Input.GetKeyDown(KeyCode.S))
         {
@@ -25,9 +33,13 @@ public class Client : MonoBehaviour
             IVehicle v = GetVehicle(requirements);
             Debug.Log(v);
         }*/
+        int.TryParse(wheelInput.text, out NumberOfWheels);
+        int.TryParse(peopleInput.text, out Passengers);
+
         NumberOfWheels = Mathf.Max(NumberOfWheels, 1);
         Passengers = Mathf.Max(Passengers, 1);
-        Engine = Cargo;
+        Engine = EngineToggle.isOn;
+        Cargo = CargoToggle.isOn;
 
         VehicleRequirements requirements = new VehicleRequirements();
         requirements.NumberOfWheels = NumberOfWheels;
@@ -36,6 +48,8 @@ public class Client : MonoBehaviour
 
         IVehicle v = GetVehicle(requirements);
         Debug.Log(v);
+
+        vehicleName.text = v.GetType().Name;
     }
 
     private static IVehicle GetVehicle(VehicleRequirements requirements)
